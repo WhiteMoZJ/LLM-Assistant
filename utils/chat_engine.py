479a867528125f0d8a65d59d3fb3074aa0787a46
jun <whitemozj@outlook.com>
2025-02-04 20:44:28 +0800

    Refactor messages of chat response and renamed some files

--- utils/chat_engine.py
+++ utils/chat_engine.py
@@ -1,20 +1,17 @@
-import shutil
 import json
+import shutil
 
 from openai import OpenAI
 
-from .search_request import retrieve_documents
-from .search_tool import ONLINE_SEARCH_TOOL
+from .retriever import retrieve_documents
+from .retriever_lists import ONLINE_SEARCH_TOOL
 from .spinner import Spinner
 
 class ChatEngine:
     def __init__(self):
-        # presets = os.listdir('presets')
-        # preset = json.loads(open("presets/" + presets[0], "r").read())
         self.client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
         self.model = "deepseek-r1-distill-llama-8b"
-        # self.messages= [{"role": "system", "content": preset['operation']['fields'][5]['value']}]
-        self.messages= [{"role": "system", "content": "你是一个AI管家，语言风格有趣，可以带emoji表情，主要回答用户疑问，必要时总结用户输入文本内容，如果文本中出现转义字符错误，不予理会，不需要向用户解释你的功能"}]
+        self.messages= [{"role": "system", "content": "你是一个AI助手，语言风格有趣，可以带emoji表情，主要回答用户疑问。"}]
 
         print(
             "Assistant: "
@@ -23,12 +20,13 @@ class ChatEngine:
         print("(Type 'quit' to exit)")
 
     def generate_response(self, query):
-        self.messages.append({"role": "user", "content": query})
+        messages = self.messages
+        searchmessages = [{"role": "system", "content": "你是一个AI助手"}, {"role": "user", "content": query}]
         print("\nAssistant:", end=" ", flush=True)
         with Spinner("Thinking..."):
             response = self.client.chat.completions.create(
                     model=self.model,
-                    messages=self.messages,
+                    messages=searchmessages,
                     temperature=0.7,
                     max_tokens=512,
                     tools=[ONLINE_SEARCH_TOOL],
@@ -37,8 +35,13 @@ class ChatEngine:
             if response.choices[0].message.tool_calls:
                 # Handle all tool calls
                 tool_calls = response.choices[0].message.tool_calls
+                # [ChatCompletionMessageToolCall(id='536605354', 
+                #                                function=Function(arguments='{"search_query":"XXX"}', 
+                #                                name='retrieve_documents'), 
+                #                                type='function')
+
                 # Add all tool calls to messages
-                self.messages.append(
+                messages.append(
                         {
                             "role": "assistant",
                             "tool_calls": [
@@ -51,40 +54,44 @@ class ChatEngine:
                             ],
                         }
                 )
-
                 for tool_call in tool_calls:
                     try:
                         args = json.loads(tool_call.function.arguments)
                         # Retrieve documents (context) from the search engine
                         result = retrieve_documents(args["search_query"])
 
-                        # Print the ontent in a formatted way
-                        # if result["status"] == "success":
-                        #     terminal_width = shutil.get_terminal_size().columns
-                        #     print("\n" + "=" * terminal_width)
-                        #     print("-" * terminal_width)
-                        #     print(result["content"])
-                        #     print("=" * terminal_width + "\n")
+                        if result["status"] == "success":
+                            messages.append(
+                                    {
+                                        "role": "assistant",
+                                        "content": result["content"],
+                                        "tool_call_id": tool_call.id,
+                                    }
+                            )
+                            # Print the ontent in a formatted way
+                            # terminal_width = shutil.get_terminal_size().columns
+                            # print("\n" + "=" * terminal_width)
+                            # print("-" * terminal_width)
+                            # print(result["content"])
+                            # print("=" * terminal_width + "\n")
 
-                        # self.messages.append(
-                        #         {
-                        #             "role": "tool",
-                        #             "content": json.dumps(result),
-                        #             "tool_call_id": tool_call.id,
-                        #         }
-                        # )
-                        self.messages.append(
-                                {
-                                    "role": "assistant",
-                                    "content": f"Context:\n{result['content']}\n\nQuestion: {query}\n"
-                                }
-                        )
+                        else:
+                            messages.append(
+                                    {
+                                        "role": "assistant",
+                                        "content": "网络错误，请稍后再试。",
+                                        "tool_call_id": tool_call.id,
+                                    }
+                            )
                     except Exception as e:
                         continue
+            else:
+                messages = self.messages
+            messages.append({"role": "user", "content": query})
 
             stream_response = self.client.chat.completions.create(
                     model=self.model,
-                    messages=self.messages,
+                    messages=messages,
                     temperature=0.8,
                     max_tokens=4096,
                     stream=True
@@ -100,6 +107,7 @@ class ChatEngine:
                     print(content, end="", flush=True)
                     think_content += content
                 else:
+                    content = chunk.choices[0].delta.content
                     print(content, end="", flush=True)
                     dialog_content += content
         print()  # New line after streaming completes
